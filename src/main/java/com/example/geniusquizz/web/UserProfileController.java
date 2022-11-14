889470c2b1e697b7ae46db7aa1d54e6f1aec14b9
Soufiane Dribel <soufiane_dri130@hotmail.fr>
2022-11-14 10:43:20 +0100

    Mise à jour du profil 2

--- src/main/java/com/example/geniusquizz/web/UserProfileController.java
+++ src/main/java/com/example/geniusquizz/web/UserProfileController.java
@@ -6,7 +6,10 @@ import com.example.geniusquizz.repository.UserRepository;
 import com.example.geniusquizz.service.UserService;
 import com.example.geniusquizz.web.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
 
 @Controller
 public class UserProfileController {
@@ -48,6 +54,13 @@ public class UserProfileController {
                 (user.getEmail().isEmpty()))
         {
             return "redirect:/profile?error";
+        }
+
+        Optional<User> userFind = userRepository.findById(user.getId());
+
+        if (!Objects.equals(userFind.get().getEmail(), user.getEmail()))
+        {
+            return "redirect:/profile?changeEmail";
         }
 
         userService.updateAccount(user);
